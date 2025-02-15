9c26c8353b9ffd36418d74be4bb40e43a052b88e
bashirzadehdanial <bashirzadehdanial@gmail.com>
2025-02-15 15:36:21 +0000

    Add some basic functionalities

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-// This is a placeholder file which shows how you can access JSON data defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
-
 import words from "./words.json" with { type: "json" };
 
+
+const test = "Hello I am Danial"
+
+const spellCheckButton = document.getElementById("spellCheck-button")
+spellCheckButton.addEventListener("click",()=>{
+   spellCheck(test)
+}
+)
+
+function spellCheck(test){
+    const misspelledContainer = []
+    const textToLowerCase = test.toLowerCase().split(' ')
+    for(let i = 0; i<= textToLowerCase.length ;i++){
+        if(!words.includes(textToLowerCase[i])){
+           misspelledContainer.push(textToLowerCase[i]) 
+    }
+    }
+    console.log(`${misspelledContainer}misspelled`);
+    
+}
+    
+
+
 window.onload = function() {
-    document.querySelector("body").innerText = `There are ${words.length} words in the Basic English dictionary`;
+   
 }
